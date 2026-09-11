42d80a1b40382ad7129e6d2185a19ef0eab413fe
Tobi <tobi@rheia.fritz.box>
2026-09-11 15:28:06 +0200

    build: only use AppImage for Linux (jpackage deb fails)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -573,7 +573,7 @@ compose.desktop {
 
             if (os.isLinux || buildFullPlatform) {
                 linux {
-                    targetFormats(TargetFormat.Deb, TargetFormat.AppImage)
+                    targetFormats(TargetFormat.AppImage)
 
                     modules("jdk.security.auth")
 
